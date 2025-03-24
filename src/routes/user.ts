9e0b62304f514dc7ee6eb49e5659fd7ac8e897f8
unknown <roninc32@gmail.com>
2025-03-24 16:09:26 +0800

    Added user listing functionality

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -5,6 +5,14 @@ import { User } from "../entities/User";
 const router = Router();
 const userRepository = AppDataSource.getRepository(User);
 
-
+// Get all users
+router.get("/", async (req: Request, res: Response) => {
+  try {
+    const users = await userRepository.find();
+    return res.json(users);
+  } catch (error) {
+    return res.status(500).json({ message: "Error retrieving users" });
+  }
+});
 
 export default router;
